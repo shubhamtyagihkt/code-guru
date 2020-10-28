d528f3cd65ad82a1b99033454e8310697f026564
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-10-28 19:45:00 +0530

    added blog theme

---
+++ models/posts.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema  = new Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{
+    timestamps: true
+});
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+const Posts = mongoose.model('Post', postSchema);
+
+module.exports = Posts;
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET posts listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/posts', { title: 'Express', layout: 'layouts/admin'});
+  res.render('admin/posts', { title: 'Posts | Admin Panel', layout: 'layouts/admin'});
 });
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.render('admin/users', { title: 'Posts | Admin Panel', layout: 'layouts/admin'});
+});
+
+/* GET categories listing. */
+router.get('/categories', function(req, res, next) {
+  res.render('admin/categories', { title: 'Posts | Admin Panel', layout: 'layouts/admin'});
+});
+
 
 module.exports = router;
